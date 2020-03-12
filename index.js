7a3567f19957486dd23ef4780d3755df3ca0f611
MAYUR SONAWANE <mavericksonawane@gmail.com>
2020-03-12 19:07:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+//By Mayur Sonawane
+//Github- mavericksonawane
+alert('Welcome Song Maker :)')
+window.addEventListener("load", () => {
+  const sounds = document.querySelectorAll(".sound");
+  const pads = document.querySelectorAll(".pads div");
+  const visual = document.querySelector(".visual");
+  const colors = [
+    "red",
+    "blue",
+    "green",
+    "purple",
+    "#b5e604",
+    "#ec1198"
+  ];
+
+  pads.forEach((pad, index) => {
+    pad.addEventListener("click", function() {
+      sounds[index].currentTime = 0;
+      sounds[index].play();
+      createBubble(index);
+    });
+  });
+
+  const createBubble = index => {
+    //Create bubbles
+    const bubble = document.createElement("div");
+    visual.appendChild(bubble);
+    bubble.style.backgroundColor = colors[index];
+    bubble.style.animation = `jump 1s ease`;
+    bubble.addEventListener("animationend", function() {
+      visual.removeChild(this);
+    });
+  };
+});
